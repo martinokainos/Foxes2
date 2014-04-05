4fa58ec792303b756b38a24998f94e7894ba223e
martinokainos <martin.oneill@kainos.com>
2014-04-05 15:00:47 +0100

    Foxes 2

---
+++ Foxes2/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Foxes2.Startup))]
+namespace Foxes2
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
